0f522f6f86c2f7e42de6f199da2a0dd4ed3341e9
MatijaKrigl <82812023+MatijaKrigl@users.noreply.github.com>
2024-05-12 13:42:01 +0200

    Add files via upload
    Ulomek in UlomekProgram sta najpomembnejši datoteki.

---
+++ Ulomek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NALOGE8
+{
+    internal class Ulomek
+    {
+        // podatki
+        private int _stevec;
+        private int _imenovalec;
+
+        
+        public int Stevec
+        { // lastnosti
+            get { return _stevec; }
+            set
+            {
+                _stevec = value / SKd;
+                _imenovalec = _imenovalec / SKd}
+        }
+        public int Imenovalec
+        {
+            get { return _imenovalec; }
+            set { if (value == 0) throw new DivideByZeroException(); }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
