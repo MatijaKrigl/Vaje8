2c0ccf1396f907a4206467eac389eced3b5f1deb
MatijaKrigl <82812023+MatijaKrigl@users.noreply.github.com>
2024-05-12 13:48:11 +0200

    Add files via upload

---
+++ KockaProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kocka
+{
+    internal class KockaProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Ustvarimo objekt
+                RazredKocka kocka = new RazredKocka(3);
+                Console.WriteLine("Dolžina stranice kocke je enaka: " + kocka.Stranica);
+                Console.WriteLine("Površina kocke je: " + kocka.Povrsina());
+                Console.WriteLine("Volumen pa: " + kocka.Volumen());
+
+                RazredKocka kocka2 = new RazredKocka();
+                kocka2.Stranica = -2;
+                Console.WriteLine("Dolžina stranice kocke je enaka: " + kocka2.Stranica);
+                Console.WriteLine("Površina kocke je: " + kocka2.Povrsina());
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Pozor, nekaj si falil: " + ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ RazredKocka.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kocka
+{
+    internal class RazredKocka
+    {
+        private int _stranica;
+
+        public int Stranica
+        {
+            get { return _stranica; }
+            set 
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), "Dolžine ne more biti negativna!");
+                else if (value.Equals(0)) throw new ArgumentException("Kocka ne obstaja!");
+                else _stranica = value;
+            }
+        }
+
+        public RazredKocka()
+        { // konstruktor brez parametra
+            Stranica = 1;
+        }
+        public RazredKocka(int stranica) 
+        { // konstruktor
+            _stranica = stranica;  
+        }
+
+        public int Povrsina()
+        { return _stranica * 6; }
+        public int Volumen()
+        { 
+            return (int)Math.Pow(_stranica, 3);
+        }
+        public override string ToString()
+        {
+            return "Kocka ima stranico velikosti" + _stranica;
+        }
+    }
+}
